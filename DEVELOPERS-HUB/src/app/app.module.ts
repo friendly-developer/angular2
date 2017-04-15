9d8443aeca3c5962c1c1b37d704e302d57d52396
Varun G <gvsvarun@gmail.com>
2017-04-15 22:08:23 +0530

    feat: Updated module with latest reference of dev-service

--- DEVELOPERS-HUB/src/app/app.module.ts
+++ DEVELOPERS-HUB/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DevServiceComponent } from './dev-service/dev-service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DevServiceComponent
   ],
   imports: [
     BrowserModule,
